b8a3c856621b5c94a759e891161c688f4846c0da
jisu Seo <79249376+jseo9732@users.noreply.github.com>
2021-11-08 17:32:45 +0900

    Done

---
+++ README.md
@@ -0,0 +1,3 @@
+생활코딩 "Node.js-MySQL" 강의
+
+https://youtube.com/playlist?list=PLuHgQVnccGMAicFFRh8vFFFtLLlNojWUh
---
+++ main.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var url = require('url');
+var topic = require('./lib/topic');
+
+var app = http.createServer(function(request, response){
+    var _url = request.url;
+    var pathname = url.parse(_url, true).pathname;
+    var queryData = url.parse(_url, true).query;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        topic.home(request, response);
+      } else {
+        topic.page(request, response);
+      }
+    } else if(pathname === '/create'){
+      topic.create(request, response);
+    } else if(pathname === '/create_process'){
+      topic.create_process(request, response);
+    } else if(pathname === '/update'){
+      topic.update(request, response);
+    } else if(pathname === '/update_process'){
+      topic.update_process(request, response);
+    } else if(pathname === '/delete_process'){
+      topic.delete_process(request, response);
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+});
+
+app.listen(3000);
